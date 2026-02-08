a52f19a61f5710d81fc83158c48d4a0c4c15474b
kinghelong <32517615+kinghelong@users.noreply.github.com>
2026-02-08 14:51:26 +0900

    Delete CueMaker/MAC_1206_SDK directory

--- CueMaker/MAC_1206_SDK/Clean CMake.bat
+++
@@ -1,8 +0,0 @@
-REM =========== Clean cmake files ===========
-rd /S /Q "CMakeFiles\"
-del "cmake_install.cmake"
-del "CMakeCache.txt"
-del "libMaC.so"
-del "libMAC.so.14"
-del "mac"
-del "Makefile"
